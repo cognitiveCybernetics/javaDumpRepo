3bf9444bbcf65401bf46f4bdff44b5f918f8d2ed
dipangshubukun <dip.github@icloud.com>
2014-09-14 20:46:35 +0530

    c

---
+++ JavaDump/src/com/dipangshu/java/Java4.java
@@ -0,0 +1,32 @@
+package com.dipangshu.java;
+
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Iterator;
+import java.util.List;
+
+public class Java4 {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		 List list = new ArrayList();
+		 list.add("1"); 
+		 list.add("2"); 
+		 list.add("3");
+		 System.out.println(list instanceof Iterable);
+		 
+		
+//		 for (Object obj: reverse(list)){ //Can only iterate over an array or an instance of java.lang.Iterable
+//			 System.out.print(obj + ", ");
+//		 }
+	}
+	
+	public static Iterator reverse(List list){		// we need to change return type as list
+		Collections.reverse(list);		
+		return list.iterator();
+	}
+
+}
