a6e43c754704549bbdf80ab4e93e78d657c2c998
dipangshubukun <dip.github@icloud.com>
2014-09-14 20:54:08 +0530

    xfvfxb

--- JavaDump/src/com/dipangshu/java/Generics.java
+++ JavaDump/src/com/dipangshu/java/Generics.java
@@ -30,5 +30,30 @@ public class Generics<T> {
 		String s=list.get(0);
 		Object o=list;
 	}
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((object == null) ? 0 : object.hashCode());
+		return result;
+	}
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Generics other = (Generics) obj;
+		if (object == null) {
+			if (other.object != null)
+				return false;
+		} else if (!object.equals(other.object))
+			return false;
+		return true;
+	}
+	
+	
 
 }
